32c85ac99c42552a5156ce9fdb0e61d0bff27b75
Naman Dawar <namandawar@MacBookPro.lan>
2026-03-17 00:07:22 +0530

    Fix CORS for production

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: ['https://frontend-sage-two-97.vercel.app', 'http://localhost:5174', 'http://localhost:5173'],
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth'));
